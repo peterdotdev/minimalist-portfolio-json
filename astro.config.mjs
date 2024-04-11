3491abfc7f813177e13568bf3ebe8ed8da49b53b
peterdotdev <peterdotdev@gmail.com>
2024-04-11 16:04:35 -0300

    deploys porfa

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  source: "./src",
   site: 'https://peterdotdev.github.io',
   base: 'minimalist-portfolio-json',
   integrations: [react()]
