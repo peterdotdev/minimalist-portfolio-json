805253cbe968be18fc78c59e6cb06e3307f258d3
peterdotdev <peterdotdev@gmail.com>
2024-05-21 16:36:14 -0300

    rollback

--- src/cv.d.ts
+++
@@ -1,9 +0,0 @@
-// src/cv.d.ts
-declare module "src/cv.json" {
-  export const education: {
-    institution: string;
-    startDate: string;
-    endDate: string | null;
-    area: string;
-  }[];
-}
--- src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-// src/types/global.d.ts
-declare module "src/cv.json" {
-  export const education: {
-    institution: string;
-    startDate: string;
-    endDate: string | null;
-    area: string;
-  }[];
-}
