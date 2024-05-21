38a8379dbe731ee881aaa783bce2654ff1bd072e
peterdotdev <peterdotdev@gmail.com>
2024-05-21 16:32:35 -0300

    change in typescript stuff

---
+++ src/types/global.d.ts
@@ -0,0 +1,9 @@
+// src/types/global.d.ts
+declare module "src/cv.json" {
+  export const education: {
+    institution: string;
+    startDate: string;
+    endDate: string | null;
+    area: string;
+  }[];
+}
